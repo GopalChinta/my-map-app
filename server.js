d1487125035d60144f11f12086423840ac79f909
Chintha Venu Gopal <109978977+GopalChinta@users.noreply.github.com>
2025-03-25 08:24:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const authRoutes = require("./routes/authRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api", dashboardRoutes);
